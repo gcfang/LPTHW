cbb396a468a5117707de954e2340386fe168566b
gcfang <cyrilfdd@gmail.com>
2017-03-17 23:13:42 -0700

    Ex41_0317

---
+++ Ex40_IntroToModule/MyModule.py
@@ -0,0 +1,20 @@
+# py2.7
+def apple():
+	print "I am an apple!"
+
+orange = "I am an orange!"
+
+class Song():
+	def __init__(self, lyrics):
+		self.lyrics = lyrics
+
+	def sing_me_a_song(self):
+		for line in self.lyrics:
+			print line
+
+happy_bday = Song(["Happy birthday to you",
+                   "I don't want to get sued",
+                   "So I'll stop right there"])
+
+bulls_on_parade = Song(["They rally around tha family",
+                        "With pockets full of shells"])
